a6aea89b9e6aee0aa988729a347a4171278dd956
Ian Coelho <ian.coel@hotmail.com>
2021-09-13 00:55:40 -0300

    styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import './App.css';
-import Produto from './Components/Produto';
+import styled from 'styled-components';
+
+const ProdutosContainer = styled.div`
+  display: flex;
+`;
+
+const Produto = styled.div`
+  flex: 1;
+`;
+
+const Titulo = styled.h1`
+  font-size: 2rem;
+  color: tomato;
+`;
+
+const Paragrafo = styled.p`
+  font-size: 1.5rem;
+  color: green;
+`;
+
+const Preco = styled.p`
+  background: ${({ cor }) => cor};
+  color: white;
+`;
 
 const App = () => {
   return (
     <div>
-      <Produto />
+      <ProdutosContainer>
+        <Produto>
+          <Titulo>Esse é um título</Titulo>
+          <Paragrafo>Esse é o texto</Paragrafo>
+          <Preco cor="black">2000</Preco>
+        </Produto>
+        <Produto>
+          <Titulo>Esse é um título</Titulo>
+          <Paragrafo>Esse é o texto</Paragrafo>
+          <Preco cor="red">1500</Preco>
+        </Produto>
+      </ProdutosContainer>
     </div>
   );
 };
---
+++ src/aulasCompletadas/CSSImportEModules/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Produto from './Components/Produto';
+
+const App = () => {
+  return (
+    <div>
+      <Produto />
+    </div>
+  );
+};
+
+export default App;
