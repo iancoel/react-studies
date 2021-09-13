b234ecbdbef4c45b4535ff60273413ee9d7bae7a
Ian Coelho <ian.coel@hotmail.com>
2021-09-13 01:16:58 -0300

    css frameworks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const ProdutosContainer = styled.div`
-  display: flex;
-`;
-
-const Produto = styled.div`
-  flex: 1;
-`;
-
-const Titulo = styled.h1`
-  font-size: 2rem;
-  color: tomato;
-`;
-
-const Paragrafo = styled.p`
-  font-size: 1.5rem;
-  color: green;
-`;
-
-const Preco = styled.p`
-  background: ${({ cor }) => cor};
-  color: white;
-`;
+import Card from 'react-bootstrap/Card';
 
 const App = () => {
   return (
-    <div>
-      <ProdutosContainer>
-        <Produto>
-          <Titulo>Esse é um título</Titulo>
-          <Paragrafo>Esse é o texto</Paragrafo>
-          <Preco cor="black">2000</Preco>
-        </Produto>
-        <Produto>
-          <Titulo>Esse é um título</Titulo>
-          <Paragrafo>Esse é o texto</Paragrafo>
-          <Preco cor="red">1500</Preco>
-        </Produto>
-      </ProdutosContainer>
-    </div>
+    <Card bg="dark" text="white" className="m-5" style={{ maxWidth: '18rem' }}>
+      <Card.Header>Notebook</Card.Header>
+      <Card.Body>
+        <Card.Title>R$2600</Card.Title>
+        <Card.Text>
+          este é um texto Lorem ipsum dolor sit amet consectetur, adipisicing
+          elit. Expedita reiciendis esse, repudiandae quasi sapiente dolore
+          magni ab nihil maiores. Architecto quasi quo cupiditate at autem
+          maxime ab laudantium, maiores aliquid.
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
